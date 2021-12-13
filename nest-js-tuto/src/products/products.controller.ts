18dfe2ffefed5c4e4107b7fdc15115edf4b72a4e
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2021-12-13 16:54:08 -0300

    GET e POST

--- nest-js-tuto/src/products/products.controller.ts
+++ nest-js-tuto/src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get } from "@nestjs/common";
 
 import { ProductsService } from "./products.service";
 
@@ -22,5 +22,10 @@ export class ProductsController {
         else{
             return {msg: 'Não há título'};
         }
+    }
+
+    @Get()
+    getAllProducts(){
+        return this.productsService.getProducts();
     }
 } 
