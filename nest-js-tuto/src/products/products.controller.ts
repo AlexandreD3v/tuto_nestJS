fa9a5011bec69da2320e82b68ffd46cddec71ff1
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2021-12-13 16:26:18 -0300

    First API

---
+++ nest-js-tuto/src/products/products.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Post, Body } from "@nestjs/common";
+
+import { ProductsService } from "./products.service";
+
+@Controller('products')
+export class ProductsController {
+
+    constructor(private productsService: ProductsService){
+
+    }
+    
+    @Post()
+    addProduct(
+        @Body('title') prodTitle:string,
+        @Body('desc') prodDesc:string,
+        @Body('price') prodPrice:number,
+    ) {
+        if(prodTitle){
+            const generatedId = this.productsService.insertProduct(prodTitle, prodDesc, prodPrice);
+            return {id: generatedId};
+        }
+        else{
+            return {msg: 'Não há título'};
+        }
+    }
+} 
